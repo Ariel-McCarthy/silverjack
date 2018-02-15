dd02d4445c567f8c6db811b67a7cbfd186add4ca
Andrea Lara <anlara@csumb.edu>
2018-02-15 07:03:10 +0000

    fixed issue with display of one of the players and resized an image

--- index.php
+++ index.php
@@ -7,23 +7,24 @@
             //echo "<img src ='./img/cards/clubs/2.png' />";  
             //echo "<br>";
             
-            $player1 = array(
+            $player3 = array(
                 'name' => 'andrea',
                 'imgURL' => './img/userPics/AndreaL1.jpg',
                 'hand' => array(),
-                'points' => 0);
+                'points' => 0
+                );
             $player2 = array(
                 'name' => 'celine',
                 'imgURL' => 'img/userPics/cwu.jpeg',
                 'hand' => array(),
                 'points' => 0
                 );
-            $player1 = array
-            ('name' => 'Ariel', 
-            'imgURL' => 'img/baby_me.jpg',
-            'hand' => array(),
-            'points' => 0
-            );
+            $player1 = array(
+                'name' => 'Ariel', 
+                'imgURL' => 'img/userPics/baby_me.jpg',
+                'hand' => array(),
+                'points' => 0
+                );
             $player4 = array(
                 'name' => 'anakareli',
                 'imgURL' => 'img/userPics/anakareli.jpg',

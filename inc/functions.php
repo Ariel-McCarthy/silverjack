e74161f63e407ededc25ee16683802af84d881f4
Celine Wu <cwu@csumb.edu>
2018-02-20 01:46:27 +0000

    Added function specification

---
+++ inc/functions.php
@@ -0,0 +1,32 @@
+<?php
+    function generateDeck()
+    {
+        // This function generates an array containing all the cards we want to
+        // use.
+    }
+    
+    function getHand($deck)
+    {
+        // This function takes an array containing each card of the deck and
+        // returns an array containing one hand.
+        // A hand is a set of a random number of cards whose value does not
+        // exceed 42.
+    }
+    
+    function getPoints($points, $change)
+    {
+        // This function takes an integer that represents the points won and
+        // another integer whose value determine if we change the static variable
+        // returned.
+    }
+    
+    function displayHand()
+    {
+        // This function prints the hand.
+    }
+    
+    function displayWinners()
+    {
+        // This function prints the winners and the points they won.
+    }
+?>

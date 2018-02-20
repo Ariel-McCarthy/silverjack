c7292245c4f5c065186d5cd1710154b5c2b90456
sovreign <somccarthy@csumb.edu>
2018-02-20 20:42:43 +0000

    linked functions.php and style.css to index.php

--- index.php
+++ index.php
@@ -1,38 +1,12 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+        <title>Silverjack</title>
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+    </head>
     <body>
         <?php
-/*        
-        function imageResize($width, $height, $andreasPic) 
-        {
-            //takes the larger size of the width and height and applies the
-            //formula accordingly...this is so this script will work
-            //dynamically with any size image
-            
-            if ($width > $height) 
-            {
-            $percentage = ($andreasPic / $width);
-            } 
-            else 
-            {
-            $percentage = ($andreasPic / $height);
-            }
-            
-            //gets the new value and applies the percentage, then rounds the value
-            $width = round($width * $percentage);
-            $height = round($height * $percentage);
-            
-            //returns the new sizes in html image tag format...this is so you
-            //can plug this function inside an image tag and just get the
-            
-            return "width="$width" height="$height"";
-            
-        }
-        $andreasPic = getimagesize("img/userPics/AndreaL1.jpg");
-        imageResize($andreasPic[0], $andreasPic[1], 150);
-        
-*/
+            include 'inc/functions.php';
             //This line is to display clubs card
             //echo "<img src ='./img/cards/clubs/2.png' />";  
             //echo "<br>";
@@ -73,11 +47,6 @@
             
             printGameState($allPlayers);
             
-            }
         ?>
     </body>
-    </head>
 </html>
-
-
-

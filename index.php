f9c69e8a9502c39f654d203950647869706c1f2b
Andrea Lara <anlara@csumb.edu>
2018-02-21 05:11:28 +0000

    fixed issue in index file

--- index.php
+++ index.php
@@ -7,27 +7,17 @@
     <body>
         <?php
             include 'inc/functions.php';
-<<<<<<< HEAD
             
             generateDeck();
             echo "<br>";
             
-            $player1 = array(
-                'name' => 'andrea',
-                'imgURL' => './img/userPics/A.L.jpg',
-=======
-            //This line is to display clubs card
-            //echo "<img src ='./img/cards/clubs/2.png' />";  
-            //echo "<br>";
-            
-            
-            $player2 = array(
+             $player2 = array(
                 'name' => 'Andrea',
-                'imgURL' => './img/userPics/AndreaL1.jpg',
->>>>>>> d78bbc6684a583183559bcdd7448d69f02cad1bb
+                'imgURL' => 'img/userPics/AndreaL1.jpg',
                 'hand' => array(),
                 'points' => 0
                 );
+           
             $player4 = array(
                 'name' => 'Celine',
                 'imgURL' => 'img/userPics/cwu.jpeg',
@@ -46,6 +36,12 @@
                 'hand' => array(),
                 'points' => 0
                 );
+            $player2 = array(
+                'name' => 'Andrea',
+                'imgURL' => 'img/userPics/AndreaL1.jpg',
+                'hand' => array(),
+                'points' => 0
+                );
             
             $allPlayers = array(
                 
@@ -56,11 +52,9 @@
                 );
             
             printGameState($allPlayers);
-<<<<<<< HEAD
             
           
-=======
->>>>>>> d78bbc6684a583183559bcdd7448d69f02cad1bb
+
         ?>
     </body>
 </html>

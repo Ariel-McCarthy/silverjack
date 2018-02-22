c5eee3c73de25a1636edaf3c4de8c1242d3e8f88
Celine Wu <cwu@csumb.edu>
2018-02-22 05:03:54 +0000

    Added some CSS to center display, and PHP functions getPoints and displayHand.

--- inc/functions.php
+++ inc/functions.php
@@ -2,54 +2,56 @@
 /*        
     function imageResize($width, $height, $andreasPic) 
     {    
-        //takes the larger size of the width and height and applies the    
-        //formula accordingly...this is so this script will work
-        //dynamically with any size image
-            
-        if ($width > $height) 
+        // takes the larger size of the width and height and applies the
+        // formula accordingly... this is so this script will work
+        // dynamically with any size image
+        
+        if ($width > $height)
         {
             $percentage = ($andreasPic / $width);
-        } 
-        else 
+        }
+        
+        else
         {
             $percentage = ($andreasPic / $height);
         }
-            
-        //gets the new value and applies the percentage, then rounds the value
+        
+        // gets the new value and applies the percentage, then rounds the value
         $width = round($width * $percentage);
         $height = round($height * $percentage);
-            
-        //returns the new sizes in html image tag format...this is so you
-        //can plug this function inside an image tag and just get the
-            
-        return "width="$width" height="$height"";
-            
+        
+        // returns the new sizes in html image tag format... this is so you
+        // can plug this function inside an image tag and just get the
+        
+        return "width='$width' height='$height'";
     }
+    
     $andreasPic = getimagesize("img/userPics/AndreaL1.jpg");
     imageResize($andreasPic[0], $andreasPic[1], 150);
-        
 */
+    
     function printGameState($allPlayers)
     {
-        foreach($allPlayers as $player)
+        shuffle($allPlayers);
+        
+        foreach ($allPlayers as $player)
         {
-            echo "<img width='75'src='" . $player['imgURL']. "'/><br>";
-            echo $player['name'] . "<br/>";                
-        }        
-    }        
-            
+            echo "<img width='75' src='".$player['imgURL']."'/><br/>";
+            echo $player['name']."<br/>";
+        }
+    }
+    
     function generateDeck()
     {
         // This function generates an array containing all the cards we want to
         // use.
+        
         $suits = array("clubs","dimonds","spades","hearts");
-                
         $rSuite = rand(0,3);
         $rIndex = rand(1,13);
         $rNumb = ($rSuite * 13) + $rIndex;
         
-        echo"<img src='img/cards/$suits[$rSuite]/". $rIndex. ".png'/>";
-        
+        echo "<img src='img/cards/$suits[$rSuite]/".$rIndex.".png'/>";
     }
     
     function getHand($deck)
@@ -60,16 +62,30 @@
         // exceed 42.
     }
     
-    function getPoints($points, $change)
+    function getPoints($value, $change)
     {
         // This function takes an integer that represents the points won and
         // another integer whose value determine if we change the static variable
         // returned.
+        
+        static $points = 0;
+        
+        if ($change == 1)
+        {
+            $points = $value;
+        }
+        
+        return $points;
     }
     
-    function displayHand()
+    function displayHand($hand)
     {
         // This function prints the hand.
+        
+        foreach ($hand as $card)
+        {
+            echo "<img src='".$card."' />&nbsp;";
+        }
     }
     
     function displayWinners()
--- index.php
+++ index.php
@@ -2,59 +2,51 @@
 <html>
     <head>
         <title>Silverjack</title>
-        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <?php
             include 'inc/functions.php';
             
             generateDeck();
-            echo "<br>";
+            echo "<br/>";
             
-             $player2 = array(
+            $player1 = array(
                 'name' => 'Andrea',
                 'imgURL' => 'img/userPics/AndreaL1.jpg',
                 'hand' => array(),
                 'points' => 0
                 );
-           
-            $player4 = array(
+                
+            $player2 = array(
                 'name' => 'Celine',
                 'imgURL' => 'img/userPics/cwu.jpeg',
                 'hand' => array(),
                 'points' => 0
                 );
+                
             $player3 = array(
                 'name' => 'Ariel', 
                 'imgURL' => 'img/userPics/baby_me.jpg',
                 'hand' => array(),
                 'points' => 0
                 );
-            $player1 = array(
+                
+            $player4 = array(
                 'name' => 'Anakareli',
                 'imgURL' => 'img/userPics/anakareli.jpg',
                 'hand' => array(),
                 'points' => 0
                 );
-            $player2 = array(
-                'name' => 'Andrea',
-                'imgURL' => 'img/userPics/AndreaL1.jpg',
-                'hand' => array(),
-                'points' => 0
-                );
-            
-            $allPlayers = array(
                 
+            $allPlayers = array(
                 $player1,
                 $player2,
                 $player3,
                 $player4
                 );
-            
+                
             printGameState($allPlayers);
-            
-          
-
         ?>
     </body>
 </html>
